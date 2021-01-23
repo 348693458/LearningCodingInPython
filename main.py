65ca22ab431a74057fd33c240b46ad3a5565fa08
348693458 <adeng.canada06@gmail.com>
2021-01-23 20:02:30 +0000

    I did a program.

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-print("Hello World!")
-help = input("How can I help you?")
+print("Hello, welcome to my greeting program!")
+name = input("What is your name?")
+print("Hi ", name, ", nice to meet you!")
+input("Press enter to exit!")
