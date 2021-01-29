0e6ebc880bfb3938b56cc9e3ab8de313237f5282
348693458 <adeng.canada06@gmail.com>
2021-01-29 00:36:01 +0000

    Welcome to my program ^_^

--- lesson 1/evensum.py
+++ lesson 1/evensum.py
@@ -1,12 +1,14 @@
 # calculate the sum of number 1 to 100
 a = 1
 sum = 0
+print ("Hello, welcome to my program!")
+upto = int (input("Please input a number."))
 
 # using infinity loop and break statement
 while True:
     if a % 2 == 0:
         sum += a
     a += 1
-    if a > 100:
+    if a > upto:
         break
 print("Sum = {0}".format(sum))
