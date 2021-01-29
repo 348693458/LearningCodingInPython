dba0671375fa5b6de499940472df8bee75b35939
348693458 <adeng.canada06@gmail.com>
2021-01-29 02:26:06 +0000

    Printed the fibonacci in one line.

--- lesson 1/fibonacci.py
+++ lesson 1/fibonacci.py
@@ -1,11 +1,11 @@
-# calculate the fibonacci sequence up to 100
+# print the fibonacci sequence up to 100
 a = 0
 b = 1
 
 # using infinity loop and break statement
 while True:
     c = a + b
-    print (c)
+    print(c, end = " ")
     a = b
     b = c
     if c >= 100:
