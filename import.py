bc3e77237730031106ca4ec650cf7b1de9a7760e
348693458 <adeng.canada06@gmail.com>
2021-01-30 17:02:24 +0000

    I did it! Finally!!!

---
+++ import.py
@@ -0,0 +1,11 @@
+import random
+
+# the function randint in random module
+# will return a random number in the given 
+# range.
+print(random.randint(0, 10))
+
+a = 0
+while a != 8:
+    a = random.randint(0, 10)
+    print (a)
--- lesson 1/guessnumber.py
+++ lesson 1/guessnumber.py
@@ -1,2 +1,15 @@
 print("Welcome to the guess number game")
 print("Program picking number...")
+import random
+a = random.randint(0, 10)
+b = int (input("Please input a number within the range of 1 to 10."))
+while True:
+    b != a 
+    if b > a:
+        print("The number is too high!")
+    b = int (input("Please input a number within the range of 1 to 10."))
+    if  b < a:
+        print("The number is too low!")
+    b = int (input("Please input a number within the range of 1 to 10."))    
+    break
+print("Correct! Game over.")
